bfeee3c0ad6c701fa33444b6f816f5431f09908f
André Felipe Souza Dias <andrefelipesouzadias@gmail.com>
2026-05-15 15:42:50 -0300

    Add files via upload

---
+++ processador.php
@@ -0,0 +1,51 @@
+<?php
+
+  //print_r($_REQUEST);
+
+
+  function somatorio(int $n){
+
+    $somatorio = 0;
+
+    for ($i=1; $i < $n; $i++) { 
+        $somatorio += $i;
+    }
+  return $somatorio;
+  
+  
+  }
+
+function somaDigitos(int $numero)
+{
+
+    $soma = 0;
+
+    while ($numero > 0){
+        $digito = $numero % 10;
+        $soma += $digito;
+        $numero = $numero / 10;
+    }
+
+    return $soma;
+
+}
+
+function safadometro(int $dia, int $mes, int $ano){
+
+global $anjo;
+global $safadesa;
+
+ $safadesa = somatorio($mes) + somaDigitos($ano) * (50 - $dia) / 10;
+ $anjo = 100 - $safadesa;
+ 
+ print "seu grau de anjo perfeito é: $anjo%. mas aquele $safadesa% é de não anjo.";
+
+}
+
+  $dia = $_REQUEST['campo_dia'];
+  $mes = $_REQUEST['campo_mes'];
+  $ano = $_REQUEST['campo_ano'];
+
+  safadometro($dia, $mes, $ano);
+
+?>
